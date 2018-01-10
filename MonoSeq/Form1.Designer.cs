49211a39a809999f2d58256c99e36ebd35038060
Gregor Schulte <gregor.schulte@uni-tuebingen.de>
2018-01-10 13:08:22 +0100

    Initial Commit

--- MonoSeq/Form1.Designer.cs
+++ MonoSeq/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MonoSeq
 {
-    partial class Form1
+    partial class frmMonoSeq
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnStartRun = new System.Windows.Forms.Button();
+            this.chartSpectrum = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartSpectrum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 591);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(37, 13);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Status";
+            // 
+            // btnStartRun
+            // 
+            this.btnStartRun.Location = new System.Drawing.Point(12, 541);
+            this.btnStartRun.Name = "btnStartRun";
+            this.btnStartRun.Size = new System.Drawing.Size(75, 23);
+            this.btnStartRun.TabIndex = 1;
+            this.btnStartRun.Text = "Start";
+            this.btnStartRun.UseVisualStyleBackColor = true;
+            this.btnStartRun.Click += new System.EventHandler(this.btnStartRun_Click);
+            // 
+            // chartSpectrum
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartSpectrum.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartSpectrum.Legends.Add(legend1);
+            this.chartSpectrum.Location = new System.Drawing.Point(15, 13);
+            this.chartSpectrum.Name = "chartSpectrum";
+            this.chartSpectrum.Size = new System.Drawing.Size(898, 462);
+            this.chartSpectrum.TabIndex = 2;
+            this.chartSpectrum.Text = "chartSpectrum";
+            // 
+            // frmMonoSeq
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(925, 613);
+            this.Controls.Add(this.chartSpectrum);
+            this.Controls.Add(this.btnStartRun);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "frmMonoSeq";
+            this.Text = "MonoSeq";
+            this.Load += new System.EventHandler(this.frmMonoSeq_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartSpectrum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnStartRun;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartSpectrum;
     }
 }
 
--- MonoSeq/Form1.cs
+++ MonoSeq/Form1.cs
@@ -7,14 +7,105 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
 
 namespace MonoSeq
 {
-    public partial class Form1 : Form
+    public partial class frmMonoSeq : Form
     {
-        public Form1()
+
+        OmniDriver.CCoWrapper wrapper = new OmniDriver.CCoWrapper();
+
+        int integrationTime = 80000;                // the number of microseconds for the integration time
+        int numberOfPixels;                         // number of CCD elements/pixels provided by the spectrometer
+        int numberOfSpectrometers;                  // actually attached and talking to us
+        int selectedSpectrometer = 0;               // selected Spectrometer    
+        double[] spectrumArray, wavelengthArray;    // pixel values from the CCD elements and  wavelengths (in nanometers) corresponding to each CCD element
+
+
+        System.Data.DataTable SpectrumTable = new DataTable("SpectrumTable");
+        DataColumn column;
+        DataRow row;
+
+        public frmMonoSeq()
         {
             InitializeComponent();
+        }
+
+        private void btnStartRun_Click(object sender, EventArgs e)
+        {
+            wrapper.setIntegrationTime(selectedSpectrometer, integrationTime);
+            lblStatus.Text = wrapper.getFirmwareModel(selectedSpectrometer) + " Spectrometer selected.";
+
+            spectrumArray = (double[])wrapper.getSpectrum(selectedSpectrometer);
+
+            numberOfPixels = spectrumArray.GetLength(0);
+
+            wavelengthArray = (double[])wrapper.getWavelengths(selectedSpectrometer);
+
+            SpectrumTable.Columns.Add("Wavelenght");
+            for (int i = 0; i < spectrumArray.GetLength(0); i++)
+            {
+                row = SpectrumTable.NewRow();
+                row["Wavelenght"] = Math.Round(wavelengthArray[i], 2);
+
+                SpectrumTable.Rows.Add(row);
+            }
+
+            for (int k = 100; k < 1000; k = k + 100)
+            {
+                spectrumArray = (double[])wrapper.getSpectrum(selectedSpectrometer);
+
+                SpectrumTable.Columns.Add(k.ToString());
+                for (int i = 0; i < spectrumArray.GetLength(0); i++)
+                {
+                    SpectrumTable.Rows[i][k.ToString()] = spectrumArray[i];
+                    SpectrumTable.AcceptChanges();
+                }
+
+                chartSpectrum.Series.Add(k.ToString());
+
+                chartSpectrum.Series[k.ToString()].ChartType = SeriesChartType.Line;
+                for (int l = 0; l < spectrumArray.GetLength(0); l++)
+                {
+                    chartSpectrum.Series[k.ToString()].Points.AddXY(Math.Round(wavelengthArray[l], 0), spectrumArray[l]);
+                }
+
+
+            }
+
+
+            /////
+            //Write Datatable to file Start
+            ////
+            StringBuilder sb = new StringBuilder();
+            IEnumerable<string> columnNames = SpectrumTable.Columns.Cast<DataColumn>().Select(column => column.ColumnName);
+            sb.AppendLine(string.Join(",", columnNames));
+            foreach (DataRow row in SpectrumTable.Rows)
+            {
+                IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+                sb.AppendLine(string.Join(",", fields));
+            }
+            File.WriteAllText("C:\\temp\\test.csv", sb.ToString());
+            /////
+            //Write Datatable to file End
+            ////
+
+
+
+
+
+
+        }
+
+        private void frmMonoSeq_Load(object sender, EventArgs e)
+        {
+
+            numberOfSpectrometers = wrapper.openAllSpectrometers();                            // update number of spectrometers attached to the Computer
+            lblStatus.Text = numberOfSpectrometers.ToString() +" Spectrometer(s) found.";      // update the Status Label
+
         }
     }
 }
